a33129071f2a7ccfaee584b83968f48d51cff300
cecolby11 <cecolby11@users.noreply.github.com>
2017-05-02 10:31:30 -0500

    orm methods

--- config/orm.js
+++ config/orm.js
@@ -2,19 +2,37 @@ var connection = require('./connection.js');
 
 var orm = {
 
-  selectAll: function() {
-    connection.query('SELECT * FROM burgers', function(error, data) {
-      console.log(data);
+  selectAll: function(tableName, func) {
+    connection.query('SELECT * FROM ??', tableName,function(error, data) {
+      if(error) {
+        console.log(error);
+      } else {
+        func(data); //callback
+      }
     });
   },
 
-  insertOne: function() {
-    // TODO
+  insertOne: function(tableName, colName, value, func) {
+    connection.query('INSERT INTO ?? (??) VALUES (?) ', [tableName, colName, value], function(error, data) {
+      if(error) {
+        console.log(error);
+      } else {
+        func(data); //callback
+      }
+    })
   }, 
 
-  updateOne: function() {
+  updateOne: function(tableName, criteriaCol, criteriaVal, updateColName, newValue, func) {
     // TODO
+    connection.query('UPDATE ?? SET ?? = ? WHERE ?? = ?', [tableName, updateColName, newValue, criteriaCol, criteriaVal], function(error, data) {
+      if(error) {
+        console.log(error);
+      } else {
+        func(data);
+      }
+    });
   }
+
 };
 
 module.exports = orm;
--- models/burger.js
+++ models/burger.js
@@ -3,3 +3,16 @@ var orm = require('../config/orm.js');
 
 // TODO
 // create the code that will call the ORM functions using burger specific input for the ORM.
+orm.selectAll('burgers', function(data) {
+  console.log(data)
+});
+
+orm.insertOne('burgers', 'burger_name', 'batburger', function(data) {
+  console.log('inserted new item at id ' + data.insertId);
+});
+
+orm.updateOne('burgers', 'id', '4', 'burger_name', 'BBQ', function(data) {
+  console.log('updated!');
+})
+
+
